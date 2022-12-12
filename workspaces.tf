429c5cda83850069586cf1417e87b9561df07efe
Xavi Pola <xavi@nextail.co>
2022-12-12 17:08:50 +0100

    feat: move from this to these[0] old workspace

--- workspaces.tf
+++ workspaces.tf
@@ -1,20 +1,26 @@
 locals {
-  workspace_environment_names = var.workspace_environment_names
+  workspaces = var.workspaces
+  add_deployment_name = var.add_deployment_name
 }
 
-resource "databricks_mws_workspaces" "this" {
-  for_each        = toset(local.workspace_environment_names)
+resource "databricks_mws_workspaces" "these" {
+  for_each        = toset(local.workspaces)
   
   account_id      = var.databricks_account_id
   aws_region      = var.aws_region
   workspace_name  = each.key
-  deployment_name = each.key
+  deployment_name = local.add_deployment_name ? each.key : null
 
-  credentials_id           = databricks_mws_credentials.this.credentials_id
-  storage_configuration_id = databricks_mws_storage_configurations.this.storage_configuration_id
-  network_id               = databricks_mws_networks.this.network_id
+  credentials_id           = databricks_mws_credentials.these.credentials_id
+  storage_configuration_id = databricks_mws_storage_configurations.these.storage_configuration_id
+  network_id               = databricks_mws_networks.these.network_id
 
   token {
     comment = "Terraform"
   }
+}
+
+moved {
+  from = databricks_mws_workspaces.this
+  to   = databricks_mws_workspaces.these[0]
 }
